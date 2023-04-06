b6e3de84e8af927470696ae0d46b7eb36a3228b1
Aaditya Gagneja <aadityaarora1215@gmail.com>
2023-04-07 00:09:08 +0530

    Creating end points and JSON fi;e

--- pages/blogpost/[slug].js
+++ pages/blogpost/[slug].js
@@ -12,7 +12,7 @@ const Slug = () => {
                 <h1>Title of the Blog : {slug}</h1>
                 <hr></hr>
                 <div>
-                    Hello there myself Aaditya Gagneja pursuing computer Science And engineering from National Institute of technology , hamirpur.
+                    Hello there myself Aaditya Gagneja pursuing computer Science And engineering from National Institute of technology , hamirpur. Basically living in Hanumangarh , Rajasthan. My hobies are playing outdoor games and Computer games. 
                 </div>
             </main>
         </div>
