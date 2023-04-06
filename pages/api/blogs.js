103bf01cc4ed7b62a0f44ba04451288ceeecb525
Aaditya Gagneja <aadityaarora1215@gmail.com>
2023-04-07 00:47:04 +0530

    Adding the API endpoints

--- pages/api/blogs.js
+++ pages/api/blogs.js
@@ -1,8 +1,15 @@
 import * as fs from 'fs'
 
-export default function handler(req, res) {
-  fs.readdir("blogdata" , (err , data) => {
-    console.log(err)
-    res.status(200).json(data)
-  })
+export default async function handler(req, res) {
+  let data = await fs.promises.readdir('blogdata')
+  let allblogs = []
+  let myfile 
+
+  for(let index = 0 ; index<data.length ; index++){
+    const item = data[index];
+    myfile = await fs.promises.readFile(('blogdata/' + item) , 'utf-8')
+    allblogs.push(JSON.parse(myfile))
+  }
+
+  res.status(200).json(allblogs)
 }
--- pages/blog.js
+++ pages/blog.js
@@ -1,26 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "../styles/Blog.module.css"
 import Link from "next/link";
 
 const Blog = () => {
+    const [blogs , setBlogs] = useState([]) ;
+
+    useEffect(() => {
+        fetch('http://localhost:3000/api/blogs').then((a) => {
+            return a.json()
+        }).then(parsed => {
+            console.log(parsed)
+            setBlogs(parsed)
+        })
+    } , [])
+
     return (
         <div className={styles.container}>
             <main className={styles.main}>
-                <h2>Latest Blogs</h2>
-                <div className={styles.blogItem}>
-                    <Link href={'/blogpost/learn-javascript'}><h3>Lorem Ipsum</h3></Link>
-                    <p>The things in the blogs are beautiful. Learning NextJs is beautiful.</p>
-                </div>
-
-                <div className={styles.blogItem}>
-                    <Link href={'/blogpost/learn-javascript'}><h3>Lorem Ipsum</h3></Link>
-                    <p>The things in the blogs are beautiful. Learning NextJs is beautiful.</p>
-                </div>
-
-                <div className={styles.blogItem}>
-                    <Link href={'/blogpost/learn-javascript'}><h3>Lorem Ipsum</h3></Link>
-                    <p>The things in the blogs are beautiful. Learning NextJs is beautiful.</p>
-                </div>
+                {blogs.map((blogitem) => {
+                    return (
+                        <div key={blogitem.title} className={styles.blogItem}>
+                            <Link href={`/blogpost/${blogitem.slug}`}><h3>{blogitem.title}</h3></Link>
+                            <p>{blogitem.content}</p>
+                        </div>
+                    )
+                })}
             </main>
       </div>
     )
